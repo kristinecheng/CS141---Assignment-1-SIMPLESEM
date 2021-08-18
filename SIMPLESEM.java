c33940b2c354050e8935904f6a8c1519adb2e8da
Thundelly <thundelly0609@gmail.com>
2021-08-18 13:32:46 -0700

    Added .gitignore / Updated parse functions

--- SIMPLESEM.java
+++ SIMPLESEM.java
@@ -9,7 +9,7 @@ import java.io.PrintStream;
 import javax.lang.model.util.ElementScanner6;
 
 public class SIMPLESEM {
-	
+
 	public static class tokenType {
 		static String SET = "set";
 		static String JUMP = "jump";
@@ -20,57 +20,46 @@ public class SIMPLESEM {
 	static String studentName = "YOUR NAME";
 	static String studentID = "YOUR 8-DIGIT ID";
 	static String uciNetID = "YOUR UCI-NET ID";
-	private static int nextChar; //contains the character(or -1==EOF)
-	private static final int EOF = -1; //int value for End of File
+	private static int nextChar; // contains the character(or -1==EOF)
+	private static final int EOF = -1; // int value for End of File
 	private int number;
 
 	private static LineNumberReader in;
 	public static FileOutputStream outFile;
 	private static PrintStream fileData;
 
-	public static void main(String[] args) throws IOException
-	{
+	public static void main(String[] args) throws IOException {
 		SIMPLESEM s = new SIMPLESEM(args[0]);
 		s.parseProgram();
 	}
 
-	private void printRule(String rule)
-	{
+	private void printRule(String rule) {
 		fileData.println(rule);
 	}
 
-	
-	public SIMPLESEM(String sourceFile)
-	{
-		try 
-		{
+	public SIMPLESEM(String sourceFile) {
+		try {
 			SIMPLESEM.in = new LineNumberReader(new FileReader(sourceFile));
 			SIMPLESEM.in.setLineNumber(1);
 			nextChar = SIMPLESEM.in.read(); // read in an single character
 			outFile = new FileOutputStream(sourceFile + ".out");
 			fileData = new PrintStream(outFile);
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
-			System.err.println("init: Errors accessing source file "
-					+ sourceFile);
+			System.err.println("init: Errors accessing source file " + sourceFile);
 			System.exit(-2);
 		}
 	}
-	
-	public void parseProgram() throws IOException 
-	{
-        printRule("Program");
+
+	public void parseProgram() throws IOException {
+		printRule("Program");
 		parseStatement();
-		while (nextChar != EOF)
-		{
-			parseStatement();			 
-		}		
+		while (nextChar != EOF) {
+			parseStatement();
+		}
 	}
 
-	private void parseStatement() throws IOException 
-	{
+	private void parseStatement() throws IOException {
 		printRule("Statement");
 
 		String token = "";
@@ -95,7 +84,7 @@ public class SIMPLESEM {
 
 		token = removeCRLF(token);
 
-		// TODO: 
+		// TODO:
 		// Read each line
 		// Tokenize the line (Use regex)
 		// Keep all the tokens in a list inside each parseStatement function
@@ -103,17 +92,21 @@ public class SIMPLESEM {
 
 		if (token.equals(tokenType.SET)) {
 			parseSet(token, content);
+
 		} else if (token.equals(tokenType.JUMPT)) {
 			parseJumpt(token, content);
+
 		} else if (token.equals(tokenType.JUMP)) {
 			parseJump(token, content);
+
 		} else if (token.equals(tokenType.HALT)) {
 			parseHalt();
+
 		} else {
 			System.out.println("Expression Fail");
 		}
 	}
-	
+
 	private String removeCRLF(String str) {
 
 		str = str.replace("\r", "");
@@ -123,71 +116,69 @@ public class SIMPLESEM {
 		return str;
 	}
 
-	private void parseSet(String token, String currentLine)
-	{
+	private void parseSet(String token, String currentLine) {
+		printRule("Set");
 		// display "Set"
-		// check if READ or WRITE is the end of the line 
-		// go to expression function 
+		// check if READ or WRITE is the end of the line
+		// go to expression function
 
 		// if (token.indexOf("read") == EOF)
-		// 	parseExpression(token, currentLine);
+		// parseExpression(token, currentLine);
 
 		// if (token.indexOf("write") == EOF)
-		// 	parseExpression(token, currentLine);
+		// parseExpression(token, currentLine);
 
 		System.out.println("Parsing set");
 	}
 
-	private void parseJumpt(String token, String currentLine)
-	{
+	private void parseJumpt(String token, String currentLine) {
+		printRule("Jumpt");
 		// display "Jumpt"
-		// check three expression 
+		// check three expression
 		// ex. jumpt 8, D[0] == D[1]
-		// --> get the grammar of 8, 0, 1 
+		// --> get the grammar of 8, 0, 1
 
 		System.out.println("Parsing jumpt");
 	}
 
-	private void parseJump(String token, String currentLine)
-	{
+	private void parseJump(String token, String currentLine) {
+		printRule("Jump");
 		// display "Jump"
-		// check the expression 
+		// check the expression
 
 		System.out.println("Parsing jump");
 	}
 
-	private void parseHalt()
-	{
-		// return
-
+	private void parseHalt() {
 		System.out.println("Parsing halt");
+
+		return;
 	}
 
-	private void parseExpression(String token, String currentLine)
-	{
+	private void parseExpression(String token, String currentLine) {
+		printRule("Expression");
 		// display "Expression"
-		// find the delimiter: + - 
+		// find the delimiter: + -
 		// check the left and right teram of delimiter
 	}
 
-	private void parseTerm(String token, String currentLine)
-	{
+	private void parseTerm(String token, String currentLine) {
+		printRule("Term");
 		// display "Term"
 		// find the delimiter: * / %
-		// check the left and right factor of delimiter 
+		// check the left and right factor of delimiter
 	}
 
-	private void parseFactor(String token, String currentLine)
-	{
+	private void parseFactor(String token, String currentLine) {
+		printRule("Factor");
 		// display "Factor"
-		// check if is number -> if yes, then end 
+		// check if is number -> if yes, then end
 		// if find symbol "D", go back to expression and check the chracter D[#]
 		// # might be token[2]
 	}
 
-	private void parseNumber(String token, String currentLine)
-	{
-		// display number 
+	private void parseNumber(String token, String currentLine) {
+		printRule("Number");
+		// display number
 	}
 }
-
